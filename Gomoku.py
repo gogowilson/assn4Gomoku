b5f9731d6b3ce3039df9867d8a286ac3660d5146
gogowilson <43383783+gogowilson@users.noreply.github.com>
2020-11-27 11:50:36 +0800

    Update Gomoku.py

--- Gomoku.py
+++ Gomoku.py
@@ -1,7 +1,6 @@
-def gomoku_board():
-    for i in range(15):
-        print('%6d'%(i+1),end='')
-    print()
+import sys
+def gomoku_board(): #initialize the chessboard
+    print('     1     2     3     4     5     6     7     8     9    10    11    12    13    14    15')
     for i in range(29):
         if(i%2==0):
             i = int((i+1)/2)
@@ -21,9 +20,10 @@ def gomoku_board():
         print()
 
 def play():
-    chess = {}
-    for x in range(1, 16):
-        for  y in range(1, 16):
+    global chess
+    chess = {}   #This section refers to CDSN
+    for x in range(16):
+        for  y in range(16):
             chess[(x, y)] = '+'
 
     for i in range(15*15) :
@@ -31,8 +31,9 @@ def play():
             player = 'X'
         else:
             player = 'O'
+        print('Let %s choose the position of the chess' % player)
+        
         while True:
-            print('Let %s choose the position of the chess' % player)
             position = input('Please enter the position (with CAPITAL letter)：')
             xpos = position[0]
             x = ord(xpos)-64
@@ -42,22 +43,19 @@ def play():
             else:
                 break
  
+        # here we should reprint chessborad
         if chess[(x, y)]=="+":
-            #重画棋盘
             chess[(x, y)] = player
-            for i in range(15):
-                print("%6d"%(i+1),end='')
-            print()
+            print('     1     2     3     4     5     6     7     8     9    10    11    12    13    14    15')
             for i in range(29):
                 if(i%2==0):
                     i = int((i+1)/2)
                     print(chr(i + ord('A')) + "  ", end='')
                     for j in range(53):
                         if (j%4 == 0):
-                            x=int((i+3)/2)
+                            x= i +1
                             y=int(j/4+1)
                             print(chess[(x,y)],end='')
-                        
                         else:
                             print("-", end='')
                 else:
@@ -68,17 +66,59 @@ def play():
                         else:
                             print("  ",end='')
                 print()
-        else:
-            isture=True
-            print("您输入的位置已经有子，请重新输入！")
-        win()
+        
+        win(player)
     
-def win():
-    pass
+def win(player):
+    global chess
+    #if j<=11:
+       #if board[i][j] == board[i][j+1] == board[i][j+2] == board[i][j+3] == board[i][j+4]: whowin(i,j)
+    
+    for i in range(1, 12):
+        for j in range(1, 16):
+            if (chess[(i, j)] == chess[(i + 1, j)] == chess[(i + 2, j)] == chess[(i + 3, j)] == chess[(i + 4, j)] 
+                and chess[(i, j)] in ["X", "O"]):
+                
+                result(i,j)
+                
+             
+         
+    for i in range(1, 12):
+        for j in range(1, 12):
+            if (chess[(i, j)] == chess[(i + 1, j + 1)] == chess[(i + 2, j + 2)] == chess[(i + 3, j + 3)] == chess[(i + 4, j + 4)] 
+                and chess[(i, j)] in ["X", "O"]):
+                
+                result(i,j)
+                
+                
+    for i in range(1, 12):
+        for j in range(5, 16):
+            if (chess[(i, j)] == chess[(i +1, j - 1)] == chess[(i + 2, j - 2)] == chess[(i + 3, j - 3)] == chess[(i + 4, j - 4)]
+                and chess[(i, j)] in ["X", "O"]):
+                
+                result(i,j)
+                
+            
+    for i in range(1, 16):
+        for j in range(1, 12):
+            if (chess[(i, j)] == chess[(i, j + 1)] == chess[(i, j + 2)] == chess[(i, j + 3)] == chess[(i, j + 4)] 
+                and chess[(i, j)] in ["X", "O"]):
+                
+                result(i,j)
+                
 
+def result(i,j):
+    global chess
+    
+    if chess[(i,j)] == 'X':
+        print('X win!\nGAME OVER, BYE!')
+        sys.exit(0)
+    else:
+        print('O win!\nGAME OVER, BYE!')
+        sys.exit(0)
+    
+            
 def main():
-    x = 0
-    y = 0
     print('=========PLAY GOMOKU WITH YOUR FRIENDS !==========')
     print('Both sides use X and O respectively, X go first\nPlayers alternate in placing chess on an empty position\nThe WINNER is people who get 5 connected chess\n')
     gomoku_board()
